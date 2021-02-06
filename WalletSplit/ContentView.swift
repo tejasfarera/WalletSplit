b73cc258b833654323c76a3ca0c9cec107de9c4e
tejasfarera <tejas.farera@gmail.com>
2021-02-06 19:01:05 +0530

    Initial Commit

---
+++ WalletSplit/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  WalletSplit
+//
+//  Created by rails on 06/02/21.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
