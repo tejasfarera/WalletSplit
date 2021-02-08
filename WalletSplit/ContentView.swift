6dc1684bdb6abaf1268d0113f41096716761eea1
tejasfarera <tejas.farera@gmail.com>
2021-02-08 15:42:48 +0530

    Completed contry app on Swift UI

--- WalletSplit/ContentView.swift
+++ WalletSplit/ContentView.swift
@@ -1,16 +1,55 @@
-//
-//  ContentView.swift
-//  WalletSplit
-//
-//  Created by rails on 06/02/21.
-//
-
 import SwiftUI
 
 struct ContentView: View {
+    @State private var amount = ""
+    @State private var selectedNumberOfPeople = 0
+    @State private var selectedTip = 0
+    private let tipPercentages = [5, 10, 15, 20, 25, 30]
+    
+    private var perPersonAmount: Double {
+        let _amount = Double(amount) ?? 0
+        let numberOfPeople = Double(selectedNumberOfPeople + 2)
+        let tip = (_amount * Double(tipPercentages[selectedTip])) / 100
+        let totalAmount = _amount + tip
+        let perPerson = totalAmount / numberOfPeople
+        return perPerson
+    }
+    
     var body: some View {
-        Text("Hello, world!")
-            .padding()
+        NavigationView {
+            Form{
+                Section {
+                    TextField("Enter the amount", text: $amount)
+                        .keyboardType(.decimalPad)
+                }
+                Section {
+                    Picker(
+                        selection: $selectedNumberOfPeople,
+                        label: Text("Number of people"),
+                        content: {
+                            ForEach(2..<100) {
+                                Text($0.description)
+                            }
+                        })
+                }
+                
+                Section(header: (Text("Select tip percent"))) {
+                    Picker(
+                        selection: $selectedTip,
+                        label: Text("Picker"),
+                        content: {
+                            ForEach(0..<tipPercentages.count) { tipPercent in
+                                Text("\(tipPercentages[tipPercent]) %")
+                            }
+                        })
+                        .pickerStyle(SegmentedPickerStyle())
+                }
+                Section {
+                    Text("$ \(perPersonAmount.description)")
+                }
+            }
+            .navigationBarTitle(Text("Contry app"))
+        }
     }
 }
 
@@ -19,3 +58,7 @@ struct ContentView_Previews: PreviewProvider {
         ContentView()
     }
 }
+
+
+
+// navigation bar title
